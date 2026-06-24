77dc82fc60ba8424645d87df6b6b579063ad6679
BlackSilasJava <bsjsoftwaresolutions@gmail.com>
2026-06-24 18:33:32 +0100

    Create DataTypesDemo.java

---
+++ DataTypesDemo.java
@@ -0,0 +1,49 @@
+public class DataTypeDemo {
+
+public static void main(String[] args) {
+
+    // Integer example
+    int age = 18;
+    System.out.println("Age: " + age);
+
+    // Another integer example
+    int numberOfStudents = 89;
+    System.out.println("Number of Students: " + numberOfStudents);
+
+    // Float example
+    float temperatureFloat = 36.7f;
+    System.out.println("Float Temperature: " + temperatureFloat);
+
+    // Double example
+    double temperatureDouble = 36.76;
+    System.out.println("Double Temperature: " + temperatureDouble);
+
+    // Float and Double together
+    float varOne = 48.1f;
+    double varTwo = 74.801;
+
+    System.out.println("varOne: " + varOne);
+    System.out.println("varTwo: " + varTwo);
+
+    // Char example
+    char grade = 'B';
+    System.out.println(grade);
+
+    // String example
+    String username = "SIESolutions";
+    System.out.println(username);
+   
+    // Another String example
+    System.out.println("Hello" + " " + username);
+
+    // Boolean example
+    boolean isLoggedIn = false;
+    boolean isRainy = true;
+    System.out.println(isLoggedIn);
+    System.out.println(isRainy);
+
+    // Another boolean example     
+    System.out.println(5>10);
+    System.out.println(5<10);
+}
+}
